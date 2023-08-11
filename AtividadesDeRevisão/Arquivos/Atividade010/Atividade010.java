6d70744ac37a295fba53fb49c1de86cf0f35ac98
ArexDiniz <alexgdiniz18@gmail.com>
2023-08-11 19:47:56 -0300

    RealizaçãoDaAtividade10

---
+++ AtividadesDeRevisão/Arquivos/Atividade010/Atividade010.java
@@ -0,0 +1,75 @@
+package AtividadesDeRevisão.Arquivos.Atividade010;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Atividade010 {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\AlexG\\OneDrive\\Área de Trabalho\\3°Periodo\\Programação Orientada a Objeto (POO)\\AtividadesDeRevisão\\Arquivos\\Arq.txt";
+        File file = new File(path);
+        try {
+            Leitura(file);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void Leitura(File arquivo) throws IOException {
+        try (BufferedReader leitor = new BufferedReader(new FileReader(arquivo));) {
+            String[] numerosDoVetor = leitor.readLine().split(";");
+
+            int vetorInt[] = TransformarEmVetorDeInt(numerosDoVetor);
+            SomarVizinho(vetorInt);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public static int[] TransformarEmVetorDeInt(String[] numerosDoVetorEmString) {
+        int[] vetorDeInt = new int[numerosDoVetorEmString.length];
+        for (int i = 0; i < vetorDeInt.length; i++) {
+            vetorDeInt[i] = Integer.parseInt(numerosDoVetorEmString[i]);
+        }
+        return vetorDeInt;
+    }
+
+    public static void SomarVizinho(int[] vetor) {
+        if (vetor.length % 2 != 0) {
+            int vetorNovo[] = new int[vetor.length + 1];
+            for (int i = 0; i < vetor.length; i++) {
+                vetorNovo[i] = vetor[i];
+            }
+            System.out.println("Vetor somado");
+            vetorNovo[vetorNovo.length - 1] = vetor[vetor.length - 1];
+            int vetorSomado[] = new int[vetorNovo.length / 2];
+            int j = 1;
+            int k = 0;
+            for (int i = 0; i < vetorSomado.length; i++) {
+                vetorSomado[i] = vetorNovo[k] + vetorNovo[j];
+                j = j + 2;
+                k = k + 2;
+                System.out.print(vetorSomado[i] + "-");
+            }
+
+        } else {
+            System.out.println("Vetor somado");
+
+            int vetorSomado[] = new int[vetor.length / 2];
+            int j = 1;
+            int k = 0;
+            for (int i = 0; i < vetorSomado.length; i++) {
+                vetorSomado[i] = vetor[k] + vetor[j];
+                j = j + 2;
+                k = k + 2;
+                System.out.print(vetorSomado[i] + "-");
+            }
+        }
+
+    }
+}
