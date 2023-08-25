9b0dc33eba649716d699a773086684d8baf090be
ArexDiniz <alexgdiniz18@gmail.com>
2023-08-24 21:14:53 -0300

    AtualizaçãoDaAtividade

--- AtividadeUML/Parte2/Partida.java
+++ AtividadeUML/Parte2/Partida.java
@@ -1,103 +1,44 @@
 package AtividadeUML.Parte2;
 
-import java.util.*;
-
 public class Partida {
     String nomeEquipe1;
     String nomeEquipe2;
-    int[] pontosEquipe1 = { 97, 99, 83, 85 };
-    int[] pontosEquipe2 = { 87, 83, 51, 74 };
+    int totais[];
 
     public Partida(String nomeEquipe1, String nomeEquipe2) {
         this.nomeEquipe1 = nomeEquipe1;
         this.nomeEquipe2 = nomeEquipe2;
-        pontosEquipe1 = new int[4];
-        pontosEquipe2 = new int[4];
-
+        totais = new int[2];
     }
 
-    public void iniciarPartida() {
-        calcularQuartos();
-        int[] Totais = somatoriaDosPontos();
-        Boolean temVencedor = verificarSeTemVencedor(Totais);
-        if (temVencedor) {
-            formatadorDoPlacarSemProrrogacao(Totais);
-        } else {
-            prorrogacoes(Totais);
-        }
-    }
+    public int[] somatoriaDosPontos(int[] pontosEquipe1, int[] pontosEquipe2) {
 
-    public void calcularQuartos() {
-        int contadorDeQuartos = 1;
-        Scanner sc = new Scanner(System.in);
-        while (contadorDeQuartos - 1 < 4) {
-            System.out.println("Quantos pontos a " + nomeEquipe1 + " fez no " + contadorDeQuartos + "Q");
-            pontosEquipe1[contadorDeQuartos - 1] = sc.nextInt();
-            System.out.println("Quantos pontos a " + nomeEquipe2 + " fez no " + contadorDeQuartos + "Q");
-            pontosEquipe2[contadorDeQuartos - 1] = sc.nextInt();
-            contadorDeQuartos++;
-        }
-        sc.close();
-    }
-
-    public int[] somatoriaDosPontos() {
         int totalEquipe1 = 0;
         int totalEquipe2 = 0;
         for (int i = 0; i < pontosEquipe1.length; i++) {
             totalEquipe1 = totalEquipe1 + pontosEquipe1[i];
             totalEquipe2 = totalEquipe2 + pontosEquipe2[i];
         }
-        int totais[] = { totalEquipe1, totalEquipe2 };
+        this.totais[0] = totalEquipe1;
+        this.totais[1] = totalEquipe2;
         return totais;
 
     }
 
-    public void formatadorDoPlacarSemProrrogacao(int totais[]) {
-        System.out.println("          Q1   Q2   Q3   Q4   PR   Final");
-        System.out.print(nomeEquipe1 + " ");
-        int i = 0;
-        while (i < 4) {
-            System.out.print(pontosEquipe1[i] + "   ");
-            i++;
-        }
-        System.out.print("--  " + totais[0]);
-        i = 0;
-        System.out.println();
-        System.out.print(nomeEquipe2 + "   ");
-        while (i < 4) {
-            System.out.print(pontosEquipe2[i] + "   ");
-            i++;
-        }
-
-        System.out.print("--  " + totais[1]);
-
-    }
-
-    public Boolean verificarSeTemVencedor(int[] totais) {
-        if (totais[0] == totais[1]) {
+    public Boolean verificarSeTemVencedor() {
+        if (this.totais[0] == this.totais[1]) {
             return false;
         } else {
             return true;
         }
     }
 
-    public void prorrogacoes(int[] totais) {
-
+    public String formatarPlacar(String placarAGerar, String elemento) {
+        placarAGerar += elemento + "\t";
+        return placarAGerar;
     }
 
-    public int[] getPontosEquipe1() {
-        return pontosEquipe1;
-    }
-
-    public void setPontosEquipe1(int[] pontosEquipe1) {
-        this.pontosEquipe1 = pontosEquipe1;
-    }
-
-    public int[] getPontosEquipe2() {
-        return pontosEquipe2;
-    }
-
-    public void setPontosEquipe2(int[] pontosEquipe2) {
-        this.pontosEquipe2 = pontosEquipe2;
+    public void implementarTotal(int elemento, int time) {
+        totais[time] += elemento;
     }
 }
--- AtividadeUML/Parte2/app.java
+++ AtividadeUML/Parte2/app.java
@@ -1,5 +1,7 @@
 package AtividadeUML.Parte2;
 
+import java.util.Scanner;
+
 /*3) Uma partida de basquete é disputada por duas equipes adversárias. Suas pontuações são marcadas em 4 períodos,
 chamados de “quartos” e, ao final, ganha a partida quem marcou mais ponto na soma dos períodos. Em caso de
 empate, haverá quantas prorrogações forem necessárias até que uma equipe saia vencedora. É necessário saber o
@@ -11,8 +13,9 @@ EQUIPE2 32 25 20 20 -- 97 */
 
 public class app {
     public static void main(String[] args) {
-        String nomeEquipe1 = "Red Tiger";
-        String nomeEquipe2 = "Golians";
+        String nomeEquipe1 = "The Blues";
+        String nomeEquipe2 = "The Reds";
+        Scanner sc = new Scanner(System.in);
 
         /*
          * Scanner sc = new Scanner(System.in);
@@ -22,7 +25,52 @@ public class app {
          * nomeEquipe2 = sc.nextLine();
          */
         Partida partida = new Partida(nomeEquipe1, nomeEquipe2);
-        partida.iniciarPartida();
+        int pontosTime1[] = new int[4];
+        int pontosTime2[] = new int[4];
+        for (int i = 1; i < pontosTime1.length + 1; i++) {
+            System.out.println("Quantos pontos o " + nomeEquipe1 + " fez no " + i + "Q");
+            pontosTime1[i - 1] = sc.nextInt();
+            System.out.println("Quantos pontos o " + nomeEquipe2 + " fez no " + i + "Q");
+            pontosTime2[i - 1] = sc.nextInt();
+        }
+        int totalTimes[] = new int[2];
+        totalTimes = partida.somatoriaDosPontos(pontosTime1, pontosTime2);
 
+        Boolean temVencedor = partida.verificarSeTemVencedor();
+        String periodosEscritos = "\t\tQ1\tQ2\tQ3\tQ4\t";
+        String pontosTime1Escrito = nomeEquipe1 + "\t" + pontosTime1[0] + "\t" + pontosTime1[1] + "\t" + pontosTime1[2]
+                + "\t" + pontosTime1[3] + "\t";
+        String pontosTime2Escrito = nomeEquipe2 + "\t" + pontosTime2[0] + "\t" + pontosTime2[1] + "\t" + pontosTime2[2]
+                + "\t" + pontosTime2[3] + "\t";
+        if (temVencedor) {
+
+            periodosEscritos += "PR\tFINAL";
+            pontosTime1Escrito += "--\t" + totalTimes[0];
+            pontosTime2Escrito += "--\t" + totalTimes[1];
+            System.out.println(periodosEscritos + "\n" + pontosTime1Escrito + "\n" + pontosTime2Escrito);
+
+        } else {
+            int elemento;
+            while (!temVencedor) {
+                periodosEscritos = partida.formatarPlacar(periodosEscritos, "PR");
+
+                System.out.println("Quanto os " + nomeEquipe1 + " fez nessa prorrogação");
+                elemento = sc.nextInt();
+                pontosTime1Escrito = partida.formatarPlacar(pontosTime1Escrito, Integer.toString(elemento));
+                partida.implementarTotal(elemento, 0);
+
+                System.out.println("Quanto os " + nomeEquipe2 + " fez nessa prorrogação");
+                elemento = sc.nextInt();
+                pontosTime2Escrito = partida.formatarPlacar(pontosTime2Escrito, Integer.toString(elemento));
+                partida.implementarTotal(elemento, 1);
+
+                temVencedor = partida.verificarSeTemVencedor();
+            }
+            sc.close();
+            periodosEscritos += "PR\tFINAL";
+            pontosTime1Escrito += "--\t" + totalTimes[0];
+            pontosTime2Escrito += "--\t" + totalTimes[1];
+            System.out.println(periodosEscritos + "\n" + pontosTime1Escrito + "\n" + pontosTime2Escrito);
+        }
     }
 }
